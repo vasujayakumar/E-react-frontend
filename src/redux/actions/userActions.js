01022eee28fecd9b4708129af18bb6aa433fa7ae
ram <ramvilla997@gmail.com>
2023-09-07 02:21:24 -0400

    Major changes

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,29 +1,23 @@
+
 // src/redux/actions/userActions.js
 import axios from 'axios';
 
 export const fetchUsers = () => async (dispatch) => {
   try {
-    const response = await axios.get('http://localhost:8080/api/users');
+    const response = await axios.get('http://localhost:8080/api/users'); // Replace with your API endpoint
     dispatch({ type: 'FETCH_USERS', payload: response.data });
   } catch (error) {
     console.error('Error fetching data:', error);
   }
 };
 
-export const createUser = (username, email) => async (dispatch) => {
-  try {
-    await axios.post('http://localhost:8080/api/users', { username, email });
-    dispatch(fetchUsers()); // Fetch users after creating
-  } catch (error) {
-    console.error('Error creating user:', error);
-  }
-};
 
-export const deleteUser = (id) => async (dispatch) => {
+export const fetchPatientRegistration = () => async (dispatch) => {
   try {
-    await axios.delete(`http://localhost:8080/api/users/${id}`);
-    dispatch(fetchUsers()); // Fetch users after deleting
+    const response = await axios.get('http://localhost:8080/api/users/patients'); // Replace with your actual backend API endpoint
+    dispatch({ type: 'FETCH_PATIENT_REGISTRATION_SUCCESS', payload: response.data });
   } catch (error) {
-    console.error('Error deleting user:', error);
+    console.error('Error fetching patient registration:', error);
+    dispatch({ type: 'FETCH_PATIENT_REGISTRATION_FAILURE' });
   }
 };
