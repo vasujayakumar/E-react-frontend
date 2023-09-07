3da5a770226137b945ef44c924517ae7f3d91d35
ram <ramvilla997@gmail.com>
2023-09-07 11:23:54 -0400

    minor

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,48 +1,50 @@
 
-// src/redux/actions/userActions.js
-import axios from 'axios';
-
-export const fetchUsers = () => async (dispatch) => {
-  try {
-    const response = await axios.get('http://localhost:8080/api/users'); // Replace with your API endpoint
-    dispatch({ type: 'FETCH_USERS', payload: response.data });
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
-};
-
-
-export const fetchPatientRegistration = () => async (dispatch) => {
-  try {
-    const response = await axios.get('http://localhost:8080/api/users/patients'); // Replace with your actual backend API endpoint
-    dispatch({ type: 'FETCH_PATIENT_REGISTRATION_SUCCESS', payload: response.data });
-  } catch (error) {
-    console.error('Error fetching patient registration:', error);
-    dispatch({ type: 'FETCH_PATIENT_REGISTRATION_FAILURE' });
-  }
-};
-
-// src/redux/actions/userActions.js
+//src/redux/actions/userActions.js
 // import axios from 'axios';
-
-// const BASE_URL = 'https://e-react-node-backend-22ed6864d5f3.herokuapp.com'; // Update with your Heroku app URL
 
 // export const fetchUsers = () => async (dispatch) => {
 //   try {
-//     const response = await axios.get(`${BASE_URL}/api/users`);
+//     const response = await axios.get('http://localhost:8080/api/users'); // Replace with your API endpoint
 //     dispatch({ type: 'FETCH_USERS', payload: response.data });
 //   } catch (error) {
 //     console.error('Error fetching data:', error);
 //   }
 // };
 
+
 // export const fetchPatientRegistration = () => async (dispatch) => {
 //   try {
-//     const response = await axios.get(`${BASE_URL}/api/users/patients`);
+//     const response = await axios.get('http://localhost:8080/api/users/patients'); // Replace with your actual backend API endpoint
 //     dispatch({ type: 'FETCH_PATIENT_REGISTRATION_SUCCESS', payload: response.data });
 //   } catch (error) {
 //     console.error('Error fetching patient registration:', error);
 //     dispatch({ type: 'FETCH_PATIENT_REGISTRATION_FAILURE' });
 //   }
 // };
+
+
+
+// src/redux/actions/userActions.js
+import axios from 'axios';
+
+const BASE_URL = 'https://e-react-node-backend-22ed6864d5f3.herokuapp.com'; // Update with your Heroku app URL
+
+export const fetchUsers = () => async (dispatch) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/api/users`);
+    dispatch({ type: 'FETCH_USERS', payload: response.data });
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+export const fetchPatientRegistration = () => async (dispatch) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/api/users/patients`);
+    dispatch({ type: 'FETCH_PATIENT_REGISTRATION_SUCCESS', payload: response.data });
+  } catch (error) {
+    console.error('Error fetching patient registration:', error);
+    dispatch({ type: 'FETCH_PATIENT_REGISTRATION_FAILURE' });
+  }
+};
 
