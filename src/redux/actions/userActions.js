7a64d4de846e845fa9d75d4c43f4905e268f0afc
ram <ramvilla997@gmail.com>
2023-09-07 02:46:35 -0400

    Minor change

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -21,3 +21,28 @@ export const fetchPatientRegistration = () => async (dispatch) => {
     dispatch({ type: 'FETCH_PATIENT_REGISTRATION_FAILURE' });
   }
 };
+
+// src/redux/actions/userActions.js
+// import axios from 'axios';
+
+// const BASE_URL = 'https://e-react-node-backend-22ed6864d5f3.herokuapp.com'; // Update with your Heroku app URL
+
+// export const fetchUsers = () => async (dispatch) => {
+//   try {
+//     const response = await axios.get(`${BASE_URL}/api/users`);
+//     dispatch({ type: 'FETCH_USERS', payload: response.data });
+//   } catch (error) {
+//     console.error('Error fetching data:', error);
+//   }
+// };
+
+// export const fetchPatientRegistration = () => async (dispatch) => {
+//   try {
+//     const response = await axios.get(`${BASE_URL}/api/users/patients`);
+//     dispatch({ type: 'FETCH_PATIENT_REGISTRATION_SUCCESS', payload: response.data });
+//   } catch (error) {
+//     console.error('Error fetching patient registration:', error);
+//     dispatch({ type: 'FETCH_PATIENT_REGISTRATION_FAILURE' });
+//   }
+// };
+
