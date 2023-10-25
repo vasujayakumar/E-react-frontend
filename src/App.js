5122e2e49c5dcc054e1bda35dc61face7d470b92
ShaikWasef <wasefshaik@gmail.com>
2023-10-25 18:58:44 -0400

    feat : Add skin cancer prediction page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LandingPage from './screens/LandingPage';
-import DBConnection from './screens/DBConnection';
-import Contact from './screens/Contact';
-import AboutUs from './screens/AboutUs';
-import Searchpatient from './screens/searchpatient';
-import Searchresult from './screens/searchresult';
-import Skincancerml from './screens/skincancerml';
-import Header from './components/Header-new';
-import Footer from './components/footer-new';
-import Kidney_stone_ml from './screens/eir_kidney_stone_checker';
-import Ckdml from './screens/eir_kidney_cdk_checker';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./screens/LandingPage";
+import DBConnection from "./screens/DBConnection";
+import Contact from "./screens/Contact";
+import AboutUs from "./screens/AboutUs";
+import Searchpatient from "./screens/searchpatient";
+import Searchresult from "./screens/searchresult";
+import Header from "./components/Header-new";
+import Footer from "./components/footer-new";
+import Kidney_stone_ml from "./screens/eir_kidney_stone_checker";
+import Ckdml from "./screens/eir_kidney_cdk_checker";
+import SkinCancerMl from "./screens/skinCancerMl";
 
 function App() {
   return (
@@ -19,13 +19,13 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/searchpatient" element={<Searchpatient />} />
-        <Route path="/skincancerml" element={<Skincancerml />} />
+        <Route path="/skincancerml" element={<SkinCancerMl />} />
         <Route path="/Searchresult" element={<Searchresult />} />
         <Route path="/DBConnection" element={<DBConnection />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/AboutUs" element={<AboutUs />} />
-        <Route path="/kidneystoneml" element={<Kidney_stone_ml/>} />
-        <Route path="/chronickidneyml" element={ <Ckdml />}/>
+        <Route path="/kidneystoneml" element={<Kidney_stone_ml />} />
+        <Route path="/chronickidneyml" element={<Ckdml />} />
       </Routes>
       <Footer />
     </BrowserRouter>
@@ -33,5 +33,3 @@ function App() {
 }
 
 export default App;
-
-
--- src/screens/skincancerml.jsx
+++ src/screens/skincancerml.jsx
@@ -1,149 +1,107 @@
-import {useLocation} from 'react-router-dom';
-import {Helmet} from "react-helmet";
-import '../styles/screens/diagonostic.css';
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import "../styles/screens/skinCancerMl.css";
+import axios from "axios";
 
-function Skincancerml() {
-
-    const location = useLocation();
-  //console.log(location.state.MobileNumber);
-  const [recordList, setRecordList] = useState([]);
-  const [diagnosis, setDiagnosis] = useState('');
+import { BASE_URL } from "../constants";
+import { Button, CircularProgress } from "@material-ui/core";
+function SkinCancerMl() {
+  const patientInfo = useSelector((state) => state.patientInfo);
+  const [skinCancerData, setSkinCancerData] = useState(null);
+  const [prediction, setPrediction] = useState("");
+  const [predictionLoader, setPredictionLoader] = useState(false);
 
   useEffect(() => {
-    // Function to retrieve patient records
-    const getPatientRecords = async () => {
+    async function getSkinCancerData() {
       try {
-       // const searchParams = new URLSearchParams(window.location.search);
-        //const phoneNumber = searchParams.get('phoneNumber');
-
-        const phoneNumber =location.state.MobileNumber;
-        console.log("parm found ",phoneNumber);
-      //local backend api link (http://localhost:8080/imageRetrieveByPhoneNumber)
-        const response = await axios.post('https://e-react-node-backend-22ed6864d5f3.herokuapp.com/imageRetrieveByPhoneNumber', {
-          phoneNumber,
-          recordType: 'Skin_Images',
-        });
-
-        const { data } = response;
-        if (data.error) {
-          alert(JSON.stringify(data.error));
-        } else {
-          setRecordList(data.success);
-        }
-      } catch (error) {
-        alert(`Error: ${error.message}`);
+        const { id } = patientInfo;
+        const { data } = await axios.get(
+          `http://localhost:8080/skinCancerData/${id}`
+        );
+        setSkinCancerData(data);
+      } catch (err) {
+        console.error(err);
       }
-    };
+    }
+    getSkinCancerData();
+  }, [patientInfo]);
 
-    getPatientRecords();
-  }, []);
-
-  // Function to send the image for prediction
-  const predict = async (index) => {
-    const record = recordList[index];
+  async function predict(base64Image) {
+    setPredictionLoader(true);
     try {
-      const imageBlob = await fetch(`data:image/jpeg;base64,${record.file.buffer}`).then((response) =>
-        response.blob()
-      );
       const formData = new FormData();
-      formData.append('file', imageBlob, record.file.originalname);
-      //skincancer ml model api link deployed on heroku via git
-      const response = await axios.post('https://skincancerml-1f755bd41b5d.herokuapp.com/upload', formData, {
-        headers: { 'Content-Type': 'multipart/form-data' },
-      });
+      const binaryData = atob(base64Image);
+      const arrayBuffer = new ArrayBuffer(binaryData.length);
+      const uint8Array = new Uint8Array(arrayBuffer);
 
-      const { data } = response;
-      if (data.error) {
-        alert(JSON.stringify(data.error));
-      } else {
-        storePrediction(data, record._id);
-        const diagnosisMessage = data.message || 'No diagnosis available';
-        setDiagnosis(diagnosisMessage);
+      for (let i = 0; i < binaryData.length; i++) {
+        uint8Array[i] = binaryData.charCodeAt(i);
       }
+
+      // Create a Blob with the binary data
+      const blob = new Blob([uint8Array], { type: "image/jpeg" });
+
+      formData.append("file", blob);
+      const { data } = await axios.post(
+        "https://skin-cancer-predict-8ffaf5441a72.herokuapp.com/predict",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      setPrediction(data.class);
+      setPredictionLoader(false);
     } catch (error) {
-      alert(`Error: ${error.message}`);
+      console.error("Error:", error);
     }
-  };
+  }
 
-  // Function to store the prediction result
-  const storePrediction = async (result, id) => {
-    try {
-     // const searchParams = new URLSearchParams(window.location.search);
-     // const phoneNumber = searchParams.get('phoneNumber');
-     const phoneNumber =location.state.MobileNumber;
-      const today = new Date().toISOString();
-      const offset = new Date().getTimezoneOffset();
-
-      const variable = result.message === 'Cancer' ? 1 : 0;
-      //local backend api link (http://localhost:8080/updateDisease)
-      const response = await axios.post('https://e-react-node-backend-22ed6864d5f3.herokuapp.com/updateDisease', {
-        phoneNumber,
-        disease: 'cancers',
-        date: today,
-        prediction: variable,
-        description: 'Skin Cancer',
-        accuracy: result.accuracy || null,
-        recordType: 'Skin_Images',
-        recordId: id || null,
-      });
-
-      const { data } = response;
-      if (data.error) {
-        alert(JSON.stringify(data.error));
-      } else {
-        alert(data.success);
-      }
-    } catch (error) {
-      alert(`Error: ${error.message}`);
+  function renderPredictionCell() {
+    if (predictionLoader) {
+      return <CircularProgress />;
     }
-  };
-       
-    
-    
-    
-    return (
-
-           
-
-            <>
-            <br/>   <br/>   <br/>   <br/>
-   <center> <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Image</th>
-            <th>Record Date</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {recordList.map((record, index) => (
-            <tr key={record._id}>
-              <td>
-                <img src={`data:image/jpeg;base64,${record.file.buffer}`} alt="Skin Image" width="150" height="150" />
-              </td>
-              <td>{record.RecordDate}</td>
-              <td>
-                <button onClick={() => predict(index)}>Diagnose</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <br/>
-      <br/>
-      <div>
-        <strong>Diagnosis:</strong> {diagnosis}
-      </div>
-    </div></center>   <br/>   <br/>   <br/>   <br/>   <br/>
-
-            </>
-        )
+    if (!prediction.length && skinCancerData) {
+      return (
+        <Button
+          className="predictButton"
+          onClick={() => predict(skinCancerData.file.buffer)}
+        >
+          Predict
+        </Button>
+      );
     }
+    if (prediction.length) {
+      return prediction;
+    }
+  }
 
-    export default Skincancerml;
+  return (
+    <table className="skin-cancer-container">
+      <tr>
+        <th>Patient Information</th>
+        <th>Skin Cancer Image</th>
+        <th>Prediction</th>
+      </tr>
+      <tr className="table-contents">
+        <td>
+          {patientInfo.FName} {patientInfo.MName} {patientInfo.LName}
+        </td>
+        <td>
+          {skinCancerData && (
+            <img
+              src={`data:image/jpeg;base64,${skinCancerData.file.buffer}`}
+              alt="Skin Image"
+              width="150"
+              height="150"
+            />
+          )}
+        </td>
+        <td>{renderPredictionCell()}</td>
+      </tr>
+    </table>
+  );
+}
 
-
-
+export default SkinCancerMl;
