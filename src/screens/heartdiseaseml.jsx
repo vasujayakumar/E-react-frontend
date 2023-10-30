9d22e4fef6798ce0b8202a714c7b50dd73f0d556
Varun <varunvora24@gmail.com>
2023-10-30 17:04:49 -0400

    Added heart disease jsx and css files and updated App.js and search result.jsx files for updating heart diagnosis

---
+++ src/screens/heartdiseaseml.jsx
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import { Helmet } from "react-helmet";
+import "../styles/screens/heart-disease.css";
+import axios from "axios";
+
+function Heartdiseaseml() {
+  const location = useLocation();
+  const [diagnosis, setDiagnosis] = useState("");
+  const [heartPrediction, setHeartPrediction] = useState(null);
+  const [features, setFeatures] = useState([]);
+  const [showPatientDetails, setShowPatientDetails] = useState(false);
+  const [predictionEmoji, setPredictionEmoji] = useState(''); 
+
+  // Define the order of the features and their display names
+  const featureOrder = [
+    { apiName: 'id', displayName: 'PatientID' },
+    { apiName: 'male', displayName: 'Gender' },
+    { apiName: 'age', displayName: 'Age' },
+    { apiName: 'education', displayName: 'Education' },
+    { apiName: 'currentSmoker', displayName: 'CurrentSmoker' },
+    { apiName: 'cigsPerDay', displayName: 'CigsPerDay' },
+    { apiName: 'BPMeds', displayName: 'BPMeds' },
+    { apiName: 'prevalentStroke', displayName: 'PrevalentStroke' },
+    { apiName: 'prevalentHyp', displayName: 'PrevalentHyp' },
+    { apiName: 'diabetes', displayName: 'Diabetes' },
+    { apiName: 'BMI', displayName: 'BMI' },
+  ];
+
+  const fetchHeartDiseasePrediction = async () => {
+    setHeartPrediction(null); 
+    try {
+      const phoneNumber = location.state?.MobileNumber; 
+      const response = await axios.get(
+        `https://heart-disease-api-47316f5960cf.herokuapp.com/predict/phone/${phoneNumber}`
+      );
+      const { data } = response;
+      if (data.error) {
+        alert(JSON.stringify(data.error));
+      } else {
+        setHeartPrediction(data.prediction);
+        
+        setPredictionEmoji(data.prediction === "The patient will not develop heart disease." ? '🙂' : '🙁');
+
+        
+        const orderedFeatures = featureOrder.map(({ apiName, displayName }) => {
+          let value = data.features[apiName];
+          // Special handling for gender
+          if (apiName === 'male') {
+            value = value === 1 ? 'Male' : 'Female';
+          }
+          return {
+            key: displayName,
+            value: value,
+          };
+        });
+
+        setFeatures(orderedFeatures); 
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 404) {
+        setHeartPrediction({ error: "Record does not exist" });
+      } else {
+        setHeartPrediction({ error: `An unexpected error occurred: ${error.message}` });
+      }
+    }
+  };
+
+  const togglePatientDetails = () => {
+    setShowPatientDetails(!showPatientDetails); // Toggle the visibility of patient details
+  };
+
+  return (
+    <>
+      <Helmet>
+        <title>Heart Disease Prediction</title>
+      </Helmet>
+      <center>
+        <div className="heart-disease-container">
+          <strong>Diagnosis:</strong> {diagnosis}
+          <br />
+          <button onClick={fetchHeartDiseasePrediction}>
+            Get Heart Disease Prediction
+          </button>
+          {heartPrediction && !heartPrediction.error && (
+            <div>
+              <strong>Heart Disease Prediction:</strong> {heartPrediction} {predictionEmoji}
+              <br />
+              <button onClick={togglePatientDetails}>
+                {showPatientDetails ? "Hide Patient Details" : "Show Patient Details"}
+              </button>
+            </div>
+          )}
+          {heartPrediction && heartPrediction.error && (
+            <div>
+              <strong>Error:</strong> {heartPrediction.error}
+            </div>
+          )}
+          {showPatientDetails && features && features.length > 0 && (
+            <table>
+              <thead>
+                <tr>
+                  {features.map((feature, index) => (
+                    <th key={index}>{feature.key}</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  {features.map((feature, index) => (
+                    <td key={index}>{feature.value}</td>
+                  ))}
+                </tr>
+              </tbody>
+            </table>
+          )}
+        </div>
+      </center>
+    </>
+  );
+}
+
+export default Heartdiseaseml;
--- src/screens/searchresult.jsx
+++ src/screens/searchresult.jsx
@@ -1,172 +1,177 @@
-import {useLocation} from 'react-router-dom';
-import {Link} from 'react-router-dom';
-import {useNavigate } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import '../styles/screens/Search.css';
 
 function Searchresult() {
 
-    const navigate = useNavigate();
-    const location = useLocation();
-    let dataskin = {};
-     dataskin=location.state;
-     console.log(dataskin);
-    const goToskinml = () => navigate('/Skincancerml',{state:dataskin});
-    const goTokidneystoneml = () => navigate('/kidneystoneml',{state:dataskin});
-    const goToCkdML = () => navigate('/chronickidneyml',{state:dataskin}); 
-    const goToBreastCancerML = () => navigate('/breastcancerml',{state:dataskin});   
-    const goToThyroid_Disease_ML = () => navigate('/thyroidDiseaseml',{state:dataskin});  
-    const goToPneumoniaML = () => navigate('/Pneumoniaml',{state:dataskin});
+  const navigate = useNavigate();
+  const location = useLocation();
+  let dataskin = {};
+  dataskin = location.state;
+  console.log(dataskin);
+  const goToskinml = () => navigate('/Skincancerml', { state: dataskin });
+  const goTokidneystoneml = () => navigate('/kidneystoneml', { state: dataskin });
+  const goToCkdML = () => navigate('/chronickidneyml', { state: dataskin });
+  const goToBreastCancerML = () => navigate('/breastcancerml', { state: dataskin });
+  const goToThyroid_Disease_ML = () => navigate('/thyroidDiseaseml', { state: dataskin });
+  const goToPneumoniaML = () => navigate('/Pneumoniaml', { state: dataskin });
+  const goToheartdisease = () => navigate("/Heartdiseaseml", { state: dataskin });
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    return (
 
-            <>
-    <br></br>
-    <h2>Pateint Basic Information details</h2>
-<       div class="center">
-            <center> 
-                <table class="my_table">
-                    <tr>
-                        <td>ID</td>
-                        <td>{location.state.id}</td>
-                    
-                    </tr>
-                        <tr>
-                        <td>Name</td>
-                        <td>{location.state.FName} {location.state.LName}</td>
-                    
-                    </tr>
-                        <tr>
-                        <td>Blood Group</td>
-                        <td>{location.state.BloodGroup}</td>
-                        
-                    </tr>
-                    <tr>
-                        <td>Age</td>
-                        <td>{location.state.Age}</td>
-                        
-                    </tr>
-                    <tr>
-                        <td>Phone</td>
-                        <td>{location.state.MobileNumber}</td>
-                        
-                    </tr>
-                    <tr>
-                        <td>Email</td>
-                        <td>{location.state.EmailId}</td>
-                        
-                    </tr>
-                    <tr>
-                        <td>Address</td>
-                        <td>{location.state.Address}</td>
-                        
-                    </tr>
-                </table>
-            </center>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  return (
+
+    <>
+      <br></br>
+      <h2>Pateint Basic Information details</h2>
+      <       div class="center">
+        <center>
+          <table class="my_table">
+            <tr>
+              <td>ID</td>
+              <td>{location.state.id}</td>
+
+            </tr>
+            <tr>
+              <td>Name</td>
+              <td>{location.state.FName} {location.state.LName}</td>
+
+            </tr>
+            <tr>
+              <td>Blood Group</td>
+              <td>{location.state.BloodGroup}</td>
+
+            </tr>
+            <tr>
+              <td>Age</td>
+              <td>{location.state.Age}</td>
+
+            </tr>
+            <tr>
+              <td>Phone</td>
+              <td>{location.state.MobileNumber}</td>
+
+            </tr>
+            <tr>
+              <td>Email</td>
+              <td>{location.state.EmailId}</td>
+
+            </tr>
+            <tr>
+              <td>Address</td>
+              <td>{location.state.Address}</td>
+
+            </tr>
+          </table>
+        </center>
+      </div>
+
+
+
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link rel="stylesheet" href="style.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.css" rel="stylesheet" />
+      </head>
+
+      <section class="container">
+        <h2>Patient Specific Services</h2>
+        <div class="row">
+          <div class="service">
+            <i class="ri-macbook-line"></i>
+            <h3>Skin Cancer Dignostic </h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <button class="button button5" onClick={goToskinml}> go
+            </button>
+
+          </div>
+          <div class="service">
+            <i class="ri-store-3-line"></i>
+            <h3>Brain Dignostic</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <button class="button button5">Go</button>
+
+          </div>
+        </div>
+        <div class="row">
+          <div class="service">
+            <i class="ri-macbook-line"></i>
+            <h3>Kidney Stone Diagnosis</h3>
+            <p>94% accuracy ~ Convolutional Neural Networks</p>
+            <button class="button button5" onClick={goTokidneystoneml}>Go</button>
+          </div>
+          <div class="service">
+            <i class="ri-store-3-line"></i>
+            <h3>Chronic Kidney Disease Diagnosis</h3>
+            <p>93% accuracy~ Decision Tree</p>
+            <button class="button button5" onClick={goToCkdML}>Go</button>
+          </div>
         </div>
 
-
-
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
- 
-  <link rel="stylesheet" href="style.css" />
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
-  <link href="https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.css" rel="stylesheet" />
-</head>
-
-<section class="container">
-    <h2>Patient Specific Services</h2>
-    <div class="row">
-      <div class="service">
-        <i class="ri-macbook-line"></i>
-        <h3>Skin Cancer Dignostic </h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        <button class="button button5" onClick={goToskinml}> go
-        </button>
-       
-      </div>
-      <div class="service">
-        <i class="ri-store-3-line"></i>
-        <h3>Brain Dignostic</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        <button class="button button5">Go</button>
-        
-      </div>
-    </div>
-    <div class="row">
-      <div class="service">
-        <i class="ri-macbook-line"></i>
-        <h3>Kidney Stone Diagnosis</h3>
-        <p>94% accuracy ~ Convolutional Neural Networks</p>
-        <button class="button button5" onClick={goTokidneystoneml}>Go</button>
-      </div>
-      <div class="service">
-        <i class="ri-store-3-line"></i>
-        <h3>Chronic Kidney Disease Diagnosis</h3>
-        <p>93% accuracy~ Decision Tree</p>
-        <button class="button button5" onClick={goToCkdML}>Go</button>
-      </div>
-      </div>
-    
-    <div class="row">
-      <div class="service">
-        <i class="ri-macbook-line"></i>
-        <h3>Breast Cancer Diagnosis</h3>
-        <p>97% accuracy ~ Linear Logistic Regression</p>
-        <button class="button button5" onClick={goToBreastCancerML}>Go</button>
-      </div>
-      <div class="service">
-        <i class="ri-macbook-line"></i>
-        <h3>Thyroid Disease Detection</h3>
-        <p>96% accuracy ~ Random Forest Model</p>
-        <button class="button button5" onClick={goToThyroid_Disease_ML}>Go</button>
-      </div>
+        <div class="row">
+          <div class="service">
+            <i class="ri-macbook-line"></i>
+            <h3>Breast Cancer Diagnosis</h3>
+            <p>97% accuracy ~ Linear Logistic Regression</p>
+            <button class="button button5" onClick={goToBreastCancerML}>Go</button>
           </div>
-      <div class="row">
-        <div class="service">
-        <i class="ri-macbook-line"></i>
-        <h3>Skin Cancer Diagnosis</h3>
-        <p>Detecting skin cancer type using ML</p>
-        <button class="button button5" onClick={() => navigate('/skinCancerMLPage')}>Go</button>
-      </div>
-           <div class="service">
-        <i class="ri-macbook-line"></i>
-        <h3>Pneumonia Disease Detection</h3>
-        <p>94% using VGG19</p>
-        <button class="button button5" onClick={goToPneumoniaML}>Go</button>
-      </div> 
-    </div>
-  </section>
+          <div class="service">
+            <i class="ri-macbook-line"></i>
+            <h3>Thyroid Disease Detection</h3>
+            <p>96% accuracy ~ Random Forest Model</p>
+            <button class="button button5" onClick={goToThyroid_Disease_ML}>Go</button>
+          </div>
+        </div>
+        <div class="row">
+          <div class="service">
+            <i class="ri-macbook-line"></i>
+            <h3>Skin Cancer Diagnosis</h3>
+            <p>Detecting skin cancer type using ML</p>
+            <button class="button button5" onClick={() => navigate('/skinCancerMLPage')}>Go</button>
+          </div>
+          <div class="service">
+            <i class="ri-macbook-line"></i>
+            <h3>Pneumonia Disease Detection</h3>
+            <p>94% using VGG19</p>
+            <button class="button button5" onClick={goToPneumoniaML}>Go</button>
+          </div>
+        </div>
+          {/* Add Heart Disease Diagnostic Service */}
+          <div className="row">
+          <div className="service">
+            <i className="ri-heart-line"></i>
+            <h3>Heart Diagnostic</h3>
+            <p>Get heart disease results.</p>
+            <button className="button button5" onClick={goToheartdisease}>Go</button>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
 
+export default Searchresult;
 
-
-
-            </>
-        )
-    }
-
-    export default Searchresult;
-    
