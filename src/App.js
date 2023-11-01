fc78d06d53deecb211d18491a265a1ba37050b03
Pulkit Arora <pulkitarora28112000@gmail.com>
2023-11-01 14:36:28 -0400

    e-health Service Page

--- src/App.js
+++ src/App.js
@@ -50,6 +50,8 @@ import DoctorCalendar from './screens/Calendar/DoctorCalendar';
 import PatientCalendar from './screens/Calendar/PatientCalendar';
 import PatientBookTime from './screens/Calendar/PatientBookTime';
 import TimeSegmentDetail from './screens/Calendar/TimeSegmentDetail';
+import ServicesHomePage from './components/services/ServicesHomePage';
+
 
 
 class App extends Component {
@@ -119,7 +121,7 @@ class App extends Component {
           <Route path="/LabAdminRegistration" element={<LabAdminRegistration loadUser={this.loadUser} />} />
           <Route path="/LabApp" element={<LabApp />} />
           <Route path="/specialities" element={<Specialities />} />
-          <Route path="/services" element={<Services/>} />
+          <Route path="/services" element={<ServicesHomePage/>} />
           <Route path="/emergencyLocations" element={<EmergencyLocations />} />
           <Route path="/liverdiseaseML" element={<Liver_disease_ML />} />
           <Route path="/doctor" element={<DoctorLayout doctorInfo={{ id: 58 }} />}>
