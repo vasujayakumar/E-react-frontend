21b3a064faa40c5effdcb383ff9736542d4d026f
Vasundara <vjaya012@uottawa.ca>
2023-11-11 15:15:27 -0500

    push

---
+++ voiceassistant/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+import Header from './Header';
+import VoiceRecognition from './VoiceRecognition';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <VoiceRecognition />
+      
+    </div>
+  );
+}
+
+export default App;
