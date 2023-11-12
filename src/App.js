edde185e4aeaf14784be2bb47fcf35794f1491dc
MohammedNadeemPasha <nadeempasha9889@gmail.com>
2023-11-12 02:13:45 -0500

    Implemented monitoring online patients and cmade it compatible with various edge cases of user exiting the site,Implemented the remember me feature as well

--- src/App.js
+++ src/App.js
@@ -92,18 +92,90 @@ class App extends Component {
       email: data.email,
       startInPage: data.startInPage
     };
-    console.log(data);
     localStorage.setItem('loginData', JSON.stringify(userInfo));
     this.setState({
       user: userInfo,
     });
   }
+
+  loadTempUser = (data) =>{
+    const userInfo = {
+      type: data.type,
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      startInPage: data.startInPage
+    };
+    sessionStorage.setItem('loginData', JSON.stringify(userInfo));
+    this.setState({
+      user: userInfo,
+    });
+  }
+
   toggleSidebar = () => {
     this.setState((prevState) => ({
       isSidebarOpen: !prevState.isSidebarOpen,
     }));
   }
   clearUser = () => {
+    //https://e-react-node-backend-22ed6864d5f3.herokuapp.com/api/users/inactiveUser
+    const localStorageLoginData = localStorage.getItem('loginData');
+    const sessionStorageLoginData = sessionStorage.getItem('loginData');
+    if(localStorageLoginData && JSON.parse(localStorageLoginData).type === 'Patient'){
+    const email = JSON.parse(localStorageLoginData).email;
+    fetch('https://e-react-node-backend-22ed6864d5f3.herokuapp.com/api/users/inactiveUser',{
+          method: 'POST',
+          headers: {'Content-Type':'application/json'},
+          body: JSON.stringify({
+            email: email
+          })
+        })
+    .then(res =>{
+          if(res.status === 200){
+            localStorage.removeItem('loginData');
+          }else{
+            throw new Error('Error in processing request');
+          }
+        }).catch(error => {
+          console.error('There was an error during the fetch:', error);
+        });
+        this.setState({
+          user: {
+            type: 'NotLoggedIn',
+            id: -1,
+            name: '',
+            email: '',
+            startInPage: '',
+        }
+      });
+  }else if(sessionStorageLoginData && JSON.parse(sessionStorageLoginData).type === 'Patient'){ 
+    const email = JSON.parse(sessionStorageLoginData).email;
+    fetch('https://e-react-node-backend-22ed6864d5f3.herokuapp.com/api/users/inactiveUser',{
+          method: 'POST',
+          headers: {'Content-Type':'application/json'},
+          body: JSON.stringify({
+            email: email
+          })
+        })
+    .then(res =>{
+          if(res.status === 200){
+            sessionStorage.removeItem('loginData');
+          }else{
+            throw new Error('Error in processing request');
+          }
+        }).catch(error => {
+          console.error('There was an error during the fetch:', error);
+        });
+        this.setState({
+          user: {
+            type: 'NotLoggedIn',
+            id: -1,
+            name: '',
+            email: '',
+            startInPage: '',
+        }
+      });
+  }else{
     localStorage.removeItem('loginData');
     this.setState({
       user: {
@@ -115,6 +187,34 @@ class App extends Component {
       }
     });
   }
+  }
+  componentDidMount() {
+    window.addEventListener('beforeunload', this.handleBeforeUnload);
+  }
+  handleBeforeUnload = (e) =>{
+    const sessionStorageLoginData = sessionStorage.getItem('loginData');
+    if(sessionStorageLoginData && JSON.parse(sessionStorageLoginData).type === 'Patient'){ 
+      try {
+        const email = JSON.parse(sessionStorageLoginData).email;
+        fetch('https://e-react-node-backend-22ed6864d5f3.herokuapp.com/api/users/inactiveUser',{
+          method: 'POST',
+          headers: {'Content-Type':'application/json'},
+          body: JSON.stringify({
+            email: email
+          })
+        })
+      } catch (err) {
+        console.error('Error with signing out user', err);
+      }
+    }
+    e.preventDefault();
+    e.returnValue = '';
+    return 'Are you sure you want to leave? Your changes may not be saved.';
+  }
+  componentWillUnmount(){
+    window.removeEventListener('beforeunload', this.handleBeforeUnload);
+  }
+
   render() {
     return (
       <BrowserRouter>
@@ -126,7 +226,7 @@ class App extends Component {
         <Header clearUser={this.clearUser} user={this.state.user} />
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/LogIn" element={this.state.user.type === 'NotLoggedIn' ? <LogIn loadUser={this.loadUser} /> : <Navigate to={`${this.state.user.startInPage}`} />} />
+          <Route path="/LogIn" element={this.state.user.type === 'NotLoggedIn' ? <LogIn loadUser={this.loadUser} loadTempUser = {this.loadTempUser} /> : <Navigate to={`${this.state.user.startInPage}`} />} />
           <Route path="/SignUp" element={this.state.user.type === 'NotLoggedIn' ? <SignUp loadUser={this.loadUser} /> : <Navigate to={`${this.state.user.startInPage}`} />} />
           <Route path="/searchpatient" element={<Searchpatient />} />
           <Route path="/skincancerml" element={<Skincancerml />} />
